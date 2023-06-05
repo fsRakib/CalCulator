417f4fdc1f612a28afe4ce4d8a02b684f80b0a8a
fsRakib <fsrakiba50@gmail.com>
2023-06-05 16:08:52 +0600

    First attampt

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel Calculator;
+    private JTextField textDisplay;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new Calculator().Calculator);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
